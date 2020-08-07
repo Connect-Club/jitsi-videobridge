c0f3a33dc294a06daefd131b514da3ef69be4411
m1t1l <oomitiloo@gmail.com>
2020-08-07 10:11:41 +0400

    update unzip flags

--- packer/get_packer.sh
+++ packer/get_packer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 VERSION=1.6.0
 curl https://releases.hashicorp.com/packer/${VERSION}/packer_${VERSION}_linux_amd64.zip -o ./packer.zip
-unzip -u packer.zip
+unzip -oj packer.zip
 rm packer.zip
 ./packer version
