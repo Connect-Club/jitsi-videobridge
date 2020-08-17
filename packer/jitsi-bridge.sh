c7f434f12c9e8198c184cb8f7122918f1884310f
m1t1l <oomitiloo@gmail.com>
2020-08-17 15:38:12 +0400

    try bsdtar

--- packer/jitsi-bridge.sh
+++ packer/jitsi-bridge.sh
@@ -2,7 +2,7 @@
 set -e
 sudo apt-get update
 sleep 2
-sudo apt-get install openjdk-8-jdk openjdk-8-jre git unzip -y
+sudo apt-get install openjdk-8-jdk openjdk-8-jre git libarchive-tools -y
 java -version
 
 cd
@@ -10,9 +10,9 @@ git clone $GIT_URL
 cd jitsi-videobridge/
 git checkout $GIT_SHA
 curl https://apache-mirror.rbc.ru/pub/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.zip -o maven.zip
-unzip  maven.zip
+bsdtar -x -f  maven.zip ./
 apache-maven-3.6.3/bin/mvn clean package
-sudo unzip target/jitsi-videobridge.docker.zip -d /opt
+sudo bsdtar -x -f target/jitsi-videobridge.docker.zip -d /opt
 cd ..
 rm -rf jitsi-videobridge
 sudo apt-get purge git -y
