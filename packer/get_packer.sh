e0732c995d27e7dfa1f39fe0b05d8d353be0f1c8
Vladislav Hloboschin <hloboshin.vladislav@gmail.com>
2022-09-08 13:44:17 +0300

    remove packer and gitlab files

--- packer/get_packer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-VERSION=1.6.0
-curl https://releases.hashicorp.com/packer/${VERSION}/packer_${VERSION}_linux_amd64.zip -o ./packer.zip
-unzip -oj packer.zip
-rm packer.zip
-./packer version
